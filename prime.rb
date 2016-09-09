958d2697c3023150e8c710053a66f4caa1a9e96e
Nick Scaglione <nicholas.a.scaglione@gmail.com>
2016-09-09 18:29:40 +0000

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1 || !number.is_a?(Integer)
+    false
+  elsif number == 3 || number == 2
+    true
+  elsif (number % 2 == 0 || number % 3 == 0) && number != 3
+    false
+  else
+    greatest_test_divisor = Math.sqrt(number).floor
+    test_divisors = Array (2..greatest_test_divisor)
+    still_prime = true
+    test_divisors.each do |divisor|
+      if number % divisor == 0
+        still_prime = false
+      end
+    end
+    still_prime
+  end
+end
